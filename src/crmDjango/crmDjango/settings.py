e158f75476dfaadca23fcf60c9e042b25ea7074c
Nadeen Jamal <nadeen@Nadeens-MBP.home>
2021-06-05 19:27:37 -0400

    Moved files to src folder

--- src/crmDjango/crmDjango/settings.py
+++ src/crmDjango/crmDjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0&!8vkr(_qbj(&-u-)76xv19ge!b_*f)*nxuq84(vi(*#g1996'
+SECRET_KEY = 'django-insecure---m$8ul*lhcqguu0*k&)%gbwrwuc5zxk#pd+hepdi16qk=ry%@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
